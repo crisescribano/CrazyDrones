c0966fa5def80a6e44f89759c9f02a86c5eefea5
diego <diegomorin8@gmail.com>
2018-09-25 23:36:53 +0200

    n

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/diego/CrazyDrones/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/diego/CrazyDrones/devel;/opt/ros/kinetic".split(';') if "/home/diego/CrazyDrones/devel;/opt/ros/kinetic" != "" else []
